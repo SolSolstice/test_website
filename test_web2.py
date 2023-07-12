22aabdd5afa70077130aab2df1f1f99087d20bd6
matthewc <122582438+SolSolstice@users.noreply.github.com>
2023-07-12 13:44:37 -0400

    Add files via upload

--- test_web2.py
+++ test_web2.py
@@ -1,36 +1,45 @@
 from dash import Dash, dcc, html, Input, Output
 import pandas as pd 
 import plotly.express as px 
+import seaborn as sns
+import plotly.graph_objects as go
+
 
 
 rDF = pd.read_csv("data/redWineDF.csv")
 
 wDF = pd.read_csv("data/whiteWineDF.csv")
 
+
+
 app = Dash(__name__)
-server = app.server
 
 
 app.layout = html.Div([
-    html.Header("Graph Example", style={"fontSize":40,
+    html.Header("Graph Example (omfg it worked)", style={"fontSize":40,
                                         "textAlign":"center"}),
-    dcc.Dropdown(id="mydropdown",
-                 options=rDF["quality"].unique(),
-                 value = "6",
+    dcc.Dropdown(id="csv-dropdown",
+                 options=[
+                     
+                     {'label':'Red Wine','value':'Red Wine'},
+                     {'label':'White Wine','value':'White Wine'}
+                     ],
+                 value = 'Red Wine',
                  style={"width":"50%","margin-left":"130px","margin-top":"60px"}),
-    dcc.Graph(id="scatter_plot")
+    dcc.Graph(id="histogram")
 
 
 ])
 
-@app.callback(Output("scatter_plot","figure"),
-              Input("mydropdown","value"))
+@app.callback(Output('histogram',"figure"),
+              Input('csv-dropdown',"value"))
 
-def sync_input(quality_selection):
-    fig = px.scatter(rDF.loc[rDF["quality"]== quality_selection],
-                     x="quality",
-                     y="quality",
-                     hover_name="Oshit it worked")
+def update_histogram(selected_csv):
+    if selected_csv == 'Red Wine':
+        data = rDF
+    else:
+        data = wDF
+    fig = px.histogram(data,x='quality')
     return fig 
 
 
